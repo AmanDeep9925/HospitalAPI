44a4409d3654c224138b4c6ca3bf300695bb586b
AmanDeep9925 <amandeep58257@gmail.com>
2020-07-12 23:43:21 +0530

    Added Passport JWT

---
+++ config/passportJWTStrategy.js
@@ -0,0 +1,25 @@
+const passport = require('passport');
+const jwtStrategy = require('passport-jwt').Strategy;
+const extractJWT = require('passport-jwt').ExtractJwt;
+
+const Doctor = require('../models/doctor');
+
+let opts =  {
+    jwsFromRequest : extractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey : "thisKeyIsASecret"
+};
+
+passport.use(new jwtStrategy(opts,(payload,done)=>{
+    Doctor.findById(payload._id,(err,doctor)=>{
+        if(err){
+            console.log("Error in finding the info about doctor");
+            return;
+        }else if(doctor){
+            return done(null,user);
+        }else{
+            return done(null,false);
+        }
+    })
+}))
+
+module.exports = passport;
